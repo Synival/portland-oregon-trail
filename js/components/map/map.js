c1cf2443ef5a88db11b5f93684e45ca464aab0c5
mickeysanchez <sanchez.mickey@gmail.com>
2017-01-20 22:23:37 -0800

    initial setup

---
+++ js/components/map/map.js
@@ -0,0 +1,11 @@
+angular.module('portlandOregonTrail').component('map', {
+  templateUrl: 'js/components/map/map.html',
+  controller: mapController,
+  bindings: {
+
+  }
+});
+
+function mapController() {
+
+}
---
+++ js/components/sidescroll/sidescroll.js
@@ -0,0 +1,11 @@
+angular.module('portlandOregonTrail').component('sidescroll', {
+  templateUrl: 'js/components/sidescroll/sidescroll.html',
+  controller: sidescrollController,
+  bindings: {
+
+  }
+});
+
+function sidescrollController() {
+
+}
